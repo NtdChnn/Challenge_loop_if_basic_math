8e244e5b370e96b45229977fc3272f5ab91937a0
NtdChnn <69079640+NtdChnn@users.noreply.github.com>
2020-08-10 15:31:12 +0700

    finish

--- Challenge_loop_if_basic_math/Source.cpp
+++ Challenge_loop_if_basic_math/Source.cpp
@@ -2,27 +2,48 @@
 #include<math.h>
 int main()
 {
-	int stnum, ndnum, x, y;
-	float n, total1, total2, SD;
-	total = 0;
+	int stnum, ndnum, x;
+	double y, n, total1, total2, ave, SD;
+	total1 = 0;
+	total2 = 0;
 	n = 0;
-	scanf("%d", &stnum);
-	scanf("%d", &ndnum);
+	scanf_s("%d", &stnum);
+	scanf_s("%d", &ndnum);
 	if (stnum < ndnum)
 	{
 		x = stnum;
 		for (; x <= ndnum; x++)
 		{
 			printf("%d ", x);
-			total += x;
+			total1 += x;
+			total2 += x * x;
 			n++;
 		}
-		printf("\n%.1f", total / n);
-		for (; stnum <= ndnum; stnum++)
-		{
-			y =
-		}
+		ave = total1 / n;
+		printf("\nAverage = %.1f\n", ave);
+		SD = sqrt((n * total2 - total1 * total1) / (n * (n - 1)));
+		printf("SD = %.2f", SD);
 
+	}
+	else if (stnum > ndnum)
+	{
+		x = stnum;
+		for (; x >= ndnum; x--)
+		{
+			printf("%d ", x);
+			total1 += x;
+			total2 += x * x;
+			n++;
+		}
+		ave = total1 / n;
+		printf("\nAverage = %.1f\n", ave);
+		SD = sqrt((n * total2 - total1 * total1) / (n * (n - 1)));
+		printf("SD = %.2f", SD);
+
+	}
+	else {
+		y = stnum;
+		printf("%d\nAverage = %.1f\nSD = 0.00", stnum, y);
 	}
 	return 0;
 }
